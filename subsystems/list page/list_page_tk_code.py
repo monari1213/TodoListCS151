631f6f8d5489ab643ede3b1a50e9c66c40e6b2cc
somerscyn <144164003+somerscyn@users.noreply.github.com>
2023-11-28 18:43:43 -0500

    testing

--- subsystems/list page/list_page_tk_code.py
+++ subsystems/list page/list_page_tk_code.py
@@ -1,1 +1,13 @@
-# code af
+import tkinter as tk
+
+list_page = tk.Tk()
+list_page.geometry("1440x1024")
+list_page.title("list page")
+
+
+user_list = tk.Label(list_page, text="List 1 Name", font=("times new roman", 69, "bold")).place(x=25, y=25)
+
+# list1_button = tk.Button(user_page, text="List 1 Name", relief="sunken" ,font=("times new roman", 30))
+
+
+user_page.mainloop()
